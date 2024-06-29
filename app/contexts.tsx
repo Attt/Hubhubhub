2ef4d328a90741bca37b545cbe6404590e11d947
Attt <atpexgo@gmail.com>
2024-06-29 18:56:27 +0800

    Update...

--- app/contexts.tsx
+++ app/contexts.tsx
@@ -15,3 +15,4 @@ export const MenuGroupsContxt = createContext([] as {title: string, menus: {id: 
 export const ConfigGroupsContxt = createContext([] as {title: string, description: string, items: {key: string, name: string, placehodler?: string}[]}[])
 export const APIConfigGroupsContxt = createContext([] as {title: string, description: string, items: {key: string, name: string}[]}[])
 export const APISchemaContext = createContext("https://")
+export const APITokenContext = createContext("")
