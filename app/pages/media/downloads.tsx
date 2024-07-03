9a21b12b620b2aa8b64be8adf938351771c0eec3
Attt <atpexgo@gmail.com>
2024-07-03 22:38:10 +0800

    Update...

--- app/pages/media/downloads.tsx
+++ app/pages/media/downloads.tsx
@@ -1,17 +1,26 @@
 import { useFlipRefreshFlag, useRefreshFlag } from "@/app/reducers";
-import { useEffect } from "react";
+import { GET, getAPIUrl } from "@/app/requests";
+import { useEffect, useState } from "react";
 
 export default function Downloads() {
 
     const refreshFlag = useRefreshFlag();
     const flipRefreshFlag = useFlipRefreshFlag();
 
+    const [downloads, setDownloads] = useState([] as any[]);
+
     useEffect(() => {
         const loadData = async (afterSuccess: (data: any) => void) => {
             // TODO load donwloads
+            GET(getAPIUrl("query_all_downloads"), (data) => {
+                afterSuccess(data)
+            }, (err) => {
+                
+            })
         }
 
         loadData((data) => {
+            setDownloads(data)
             // sleep for 5 second
             setTimeout(() => {
                 flipRefreshFlag({});
