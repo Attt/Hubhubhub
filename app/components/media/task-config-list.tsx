123477b37eb7d3bdb23c5bd4c5bf7762af8bf207
Attt <atpexgo@gmail.com>
2024-07-28 14:30:34 +0800

    Update...

--- app/components/media/task-config-list.tsx
+++ app/components/media/task-config-list.tsx
@@ -88,13 +88,13 @@ export function TaskConfigListBody({ taskConfigs, configIdListRef }: { taskConfi
                         </div>
                         <div className="min-w-0 flex-auto">
                         {/* <p className="dark:text-zinc-100 text-sm leading-6 text-zinc-900">EP.{taskConfig.ep} / TYPE: {taskConfig.ep_type}</p> */}
-                        <p className="dark:text-zinc-100 text-sm font-semibold leading-6 text-zinc-900">
+                        <p className="dark:text-zinc-100 truncate break-all text-sm font-semibold leading-6 text-zinc-900">
                             {taskConfig.name}
                         </p>
                         <p className="mt-1 flex text-xs leading-5 text-zinc-500 break-all">
                             {mergeEpInfo(taskConfig.ep_list)}
                         </p>
-                        <p className="mt-1 flex text-xs leading-5 text-zinc-500 break-all">
+                        <p className="mt-1 flex text-xs truncate break-all leading-5 text-zinc-500 break-all">
                             {taskConfig.url}
                         </p>
                         </div>
