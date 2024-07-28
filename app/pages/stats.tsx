6523ef03949f83bfc078e7aac5514b36cc5a9043
Attt <atpexgo@gmail.com>
2024-07-28 16:06:38 +0800

    Update...

--- app/pages/stats.tsx
+++ app/pages/stats.tsx
@@ -12,21 +12,21 @@ function classNames(...classes: string[]) {
 
 export default function Stats() {
   return (
-    <div>
-      <h3 className="text-base font-semibold leading-6 text-gray-900">Last 30 days</h3>
-      <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
+    <div className="mx-auto max-w-8xl px-4 py-2 md:px-8 lg:px-8">
+      <h3 className="dark:text-zinc-100 text-base font-semibold leading-6 text-zinc-900">Last 30 days</h3>
+      <dl className="mt-5 grid grid-cols-1 divide-y divide-zinc-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0">
         {stats.map((item) => (
           <div key={item.name} className="px-4 py-5 sm:p-6">
-            <dt className="text-base font-normal text-gray-900">{item.name}</dt>
+            <dt className="dark:text-zinc-100 text-base font-normal text-zinc-900">{item.name}</dt>
             <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
-              <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
+              <div className="dark:text-indigo-400 flex items-baseline text-2xl font-semibold text-indigo-600">
                 {item.stat}
-                <span className="ml-2 text-sm font-medium text-gray-500">from {item.previousStat}</span>
+                <span className="ml-2 text-sm font-medium text-zinc-500">from {item.previousStat}</span>
               </div>
 
               <div
                 className={classNames(
-                  item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800',
+                  item.changeType === 'increase' ? 'dark:bg-green-900 dark:text-green-200 bg-green-100 text-green-800' : 'dark:bg-red-900 dark:text-red-200 bg-red-100 text-red-800',
                   'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0'
                 )}
               >
