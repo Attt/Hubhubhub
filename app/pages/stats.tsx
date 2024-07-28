65c8cbd59cda8ede002d46603140f4592144a0e1
Attt <atpexgo@gmail.com>
2024-07-28 17:06:22 +0800

    Update...

--- app/pages/stats.tsx
+++ app/pages/stats.tsx
@@ -28,7 +28,7 @@ export default function Stats() {
             }
         }, (err) => {
         }, toggleLoading)
-    })
+    }, [])
 
   return (
     <div className="mx-auto max-w-8xl px-4 py-2 md:px-8 lg:px-8">
